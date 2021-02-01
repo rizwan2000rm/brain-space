62c3692d40c02137158ba4b7397996f32da80770
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-02-01 18:25:11 +0530

    Initialized Project

---
+++ web/src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>Sort It</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
+        exercitationem?
+      </p>
+    </div>
+  );
+};
+
+export default Navbar;
