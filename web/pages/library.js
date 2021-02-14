baa71f5fe35004528c4a42d7715d00607caa6824
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-02-15 00:41:12 +0530

    Pocket API integration

--- web/pages/library.js
+++ web/pages/library.js
@@ -1,4 +1,6 @@
-const library = () => {
+const library = ({ library }) => {
+  console.log(library);
+
   return (
     <div>
       <h1>Library</h1>
@@ -7,3 +9,16 @@ const library = () => {
 };
 
 export default library;
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://getpocket.com/v3/get?consumer_key=${process.env.NEXT_PUBLIC_consumerKey}&access_token=${process.env.NEXT_PUBLIC_accessToken}`
+  );
+  const library = await res.json();
+
+  return {
+    props: {
+      library
+    }
+  };
+};
